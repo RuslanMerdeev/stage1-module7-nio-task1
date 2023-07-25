afc440ac4ea3fb4f77c3ecfc3d35f7b46dd48f7c
ruslanmerdeev <r.merdeev@haulmont.com>
2023-07-25 17:59:27 +0400

    [main] Filled up the FileReader.java

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,42 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
-
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        Map<String, String> map = fileToMap(file);
+        profile.setAge(Integer.parseInt(map.get("Age")));
+        profile.setName(map.get("Name"));
+        profile.setEmail(map.get("Email"));
+        profile.setPhone(Long.parseLong(map.get("Phone")));
+        return profile;
+    }
+
+    private Map<String, String> fileToMap(File file) {
+        Map<String, String> map = new HashMap<>();
+        try {
+            List<String> lines = Files.readAllLines(file.toPath());
+            for (String line : lines) {
+                String[] split = line.split(": ");
+                map.put(split[0], split[1]);
+            }
+        } catch (IOException e) {
+            throw new FileParsingException(e);
+        }
+        return map;
+    }
+
+    public static class FileParsingException extends RuntimeException {
+
+        public FileParsingException(Throwable cause) {
+            super(cause);
+        }
     }
 }
